21d15866fe4d59ac6e18d2e5f82ee2ea8b41fb51
Uswath <Uswath>
2023-07-14 22:46:53 +0530

    Added

--- traveldiaries/app1/views.py
+++ traveldiaries/app1/views.py
@@ -5,6 +5,6 @@ from app1.models import place,team
 def base(request):
     p = place.objects.all()
     t = team.objects.all()
-    return render(request, 'base.html', {'p': p, 't': t})
+    return render(request, 'index.html', {'p': p, 't': t})
 
 
